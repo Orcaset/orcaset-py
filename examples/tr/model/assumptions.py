2dcf5391d7d7758880396de1942b48e7eed10048
Jordan <263747311+jordan-orcaset@users.noreply.github.com>
2026-06-04 20:29:11 -0400

    Add TR example with initial income and cash flow models

---
+++ examples/tr/model/assumptions.py
@@ -0,0 +1,14 @@
+class Assumptions:
+    class Income:
+        product_sales_growth_rate = 0.05
+        rental_revenue_growth_rate = 0.05
+        product_cogs_margin = 0.50
+        rental_cogs_margin = 0.50
+        sga_growth_rate = 0.05
+        other_income_growth_rate = 0.05
+        income_tax_rate = 0.25
+        nci_net_income_rate = 0.05
+
+    class CashFlow:
+        depreciation_growth_rate = 0.05
+        accounts_receivable_margin = 0.10
